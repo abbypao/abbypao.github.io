48de32a79afba596cedd5a498c27482d5b5a8d4b
Abby <abbys7103@gmail.com>
2026-03-16 14:21:50 -0400

    chapter 5 proj 2

--- public/javascript/chapter5/project02/project05-02.js
+++ public/javascript/chapter5/project02/project05-02.js
@@ -10,3 +10,21 @@
       Filename: project05-02.js
 */
 
+let images = document.getElementsByTagName("img");
+let photoBucket = document.getElementById("photo_bucket");
+let photoList = document.getElementById("photo_list");
+
+for (let i = 0; i < images.length; i++) {
+    images[i].onclick = function() {
+        if (this.parentElement.id === "photo_bucket") {
+            let newItem = document.createElement("li");
+            photoList.appendChild(newItem);
+            newItem.appendChild(this);
+        } else {
+            let oldItem = this.parentElement;
+            photoBucket.appendChild(this);
+            oldItem.parentElement.removeChild(oldItem);
+        }
+    };
+}
+
