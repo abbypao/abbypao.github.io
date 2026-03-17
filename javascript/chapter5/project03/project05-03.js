0d40f6df03f8945627b1261fcdd6a275d5e95663
Abby <abbys7103@gmail.com>
2026-03-17 13:02:33 -0400

    Chapter 5

--- javascript/chapter5/project03/project05-03.js
+++ javascript/chapter5/project03/project05-03.js
@@ -10,3 +10,37 @@
       Filename: project05-03.js
 */
 
+// Elements used to build a table of contents
+let sourceDoc = document.getElementById("source_doc");
+let toc = document.getElementById("toc");
+
+// Heading generation state
+let headingCount = 1;
+const heading = "H2";
+
+// Iterate through each element in the source document
+for (
+  let node = sourceDoc.firstElementChild;
+  node !== null;
+  node = node.nextElementSibling
+) {
+  const n = node;
+
+  if (n.nodeName === heading) {
+    const anchor = document.createElement("a");
+    anchor.name = "doclink" + headingCount;
+    n.insertBefore(anchor, n.firstChild);
+
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    listItem.appendChild(link);
+
+    link.textContent = n.textContent;
+    link.href = "#doclink" + headingCount;
+
+    toc.appendChild(listItem);
+
+    headingCount++;
+  }
+}
+
